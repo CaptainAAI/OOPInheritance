4049b58b1a6f5c08815f355be4477cb887782d0d
CaptainAAI <anandaagungismail@gmail.com>
2024-05-20 11:38:28 +0700

    name of new ibu and new anak

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -5,3 +5,13 @@ using namespace std;
 #include "anak.h"
 #include "ibu.h"
 
+int main() {//agregasi
+	ibu* varIbu = new ibu("dini");
+	ibu* varIbu2 = new ibu("novi");
+	anak* varIbu = new anak("tono");
+	anak* varIbu = new anak("rini");
+	anak* varIbu = new anak("Dewi");
+
+	
+
+}
