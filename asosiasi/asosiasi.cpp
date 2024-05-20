7f6f968f8e2569ee6701c48eeb6c4f4a2242dad8
CaptainAAI <anandaagungismail@gmail.com>
2024-05-20 20:33:01 +0700

    class pasien and it's variable declaration

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -2,3 +2,16 @@
 using namespace std;
 #include<string>
 
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\" ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\" tidak ada\n";
+	}
+	void tambahDokter(dokter*);
+	void cetakDokter();
+};
