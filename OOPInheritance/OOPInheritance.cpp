35377383631af26c56269ee772f526f8345a7226
CaptainAAI <anandaagungismail@gmail.com>
2024-05-20 10:40:36 +0700

    update cout

--- OOPInheritance/OOPInheritance.cpp
+++ OOPInheritance/OOPInheritance.cpp
@@ -13,7 +13,9 @@ public:
 		//Definisi
 	}
 	~mahasiswa() {
-		cout << "Id  = " << id << endl;
+		cout << "Id   = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai= " << nilai << endl;
 	}
 };
 
